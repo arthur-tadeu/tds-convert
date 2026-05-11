2b9f45fecd76d3a3642d0e5f3e6a282038a78cd7
Arthur <arthur.t.carvalho@aluno.senai.br>
2026-05-11 20:25:27 -0300

    feat: switch to new firebase project tds-convert-v1

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  projectId: "dashboard-tds-25",
-  appId: "1:919633065833:web:d977d54d900128d5a0eff8",
-  storageBucket: "dashboard-tds-25.firebasestorage.app",
-  apiKey: "AIzaSyDKtu5IZgGr7rQ0cCulNSMpRxDheK73XYE",
-  authDomain: "dashboard-tds-25.firebaseapp.com",
-  messagingSenderId: "919633065833"
+  projectId: "tds-convert-v1-77854",
+  appId: "1:444430203937:web:ca55831d44c9b4a70ab760",
+  storageBucket: "tds-convert-v1-77854.firebasestorage.app",
+  apiKey: "AIzaSyCUmzMHFZp6Oa28WfvexydOGupD2wDgLWw",
+  authDomain: "tds-convert-v1-77854.firebaseapp.com",
+  messagingSenderId: "444430203937"
 };
 
 const app = initializeApp(firebaseConfig);
